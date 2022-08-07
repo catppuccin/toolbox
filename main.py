9de6f6e84eed3a0e0f2fbae100ce63a0bf1752e2
winston <hey@winston.sh>
2022-08-07 20:55:46 +0200

    style: reformat with black

--- main.py
+++ main.py
@@ -9,17 +9,20 @@ bg = None
 m = 0
 
 
-
-w, h = Image.open(sys.argv[1]).size # Get size
+w, h = Image.open(sys.argv[1]).size  # Get size
 
 while len(sys.argv) > 5:
-    if (sys.argv[5] == "--show"):
+    if sys.argv[5] == "--show":
         show = True
         sys.argv.pop(5)
     else:
         backg = True
         m = int(sys.argv.pop(9))
-        bg = Image.new('RGB', (w+m, h+m), (int(sys.argv[6]), int(sys.argv[7]), int(sys.argv[8])))
+        bg = Image.new(
+            "RGB",
+            (w + m, h + m),
+            (int(sys.argv[6]), int(sys.argv[7]), int(sys.argv[8])),
+        )
         sys.argv.pop(8)
         sys.argv.pop(7)
         sys.argv.pop(6)
@@ -27,20 +30,20 @@ while len(sys.argv) > 5:
 
 masks = []
 slices = [
-	[0, 0, 0, h, w/3, 0],
-	[0, h, w/3, 0, (w/3)*2, 0, w/3, h],
-	[w/3, h, (w/3)*2, 0, w, 0, (w/3)*2, h],
-	[(w/3)*2, h, w, 0, w, h]
+    [0, 0, 0, h, w / 3, 0],
+    [0, h, w / 3, 0, (w / 3) * 2, 0, w / 3, h],
+    [w / 3, h, (w / 3) * 2, 0, w, 0, (w / 3) * 2, h],
+    [(w / 3) * 2, h, w, 0, w, h],
 ]
 for slice in slices:
-	img = Image.new('RGB', (w, h), (0, 0, 0))
-	draw = ImageDraw.Draw(img)
-	draw.polygon(slice, fill = (255, 255, 255))
-	masks.append(img)
+    img = Image.new("RGB", (w, h), (0, 0, 0))
+    draw = ImageDraw.Draw(img)
+    draw.polygon(slice, fill=(255, 255, 255))
+    masks.append(img)
 
 
 def gen_masked(source, mask):
-    mask = mask.convert('L')
+    mask = mask.convert("L")
     img = Image.open(source)
     # im = Image.open('image.png')
 
@@ -51,10 +54,10 @@ def gen_masked(source, mask):
 
 
 def round_corners(im, rad=15):
-    circle = Image.new('L', (rad * 2, rad * 2), 0)
+    circle = Image.new("L", (rad * 2, rad * 2), 0)
     draw = ImageDraw.Draw(circle)
     draw.ellipse((0, 0, rad * 2, rad * 2), fill=255)
-    alpha = Image.new('L', im.size, "white")
+    alpha = Image.new("L", im.size, "white")
     w, h = im.size
     alpha.paste(circle.crop((0, 0, rad, rad)), (0, 0))
     alpha.paste(circle.crop((0, rad, rad, rad * 2)), (0, h - rad))
@@ -64,18 +67,18 @@ def round_corners(im, rad=15):
     return im
 
 
-final = Image.new('RGB', (w, h), (0, 0, 0))
+final = Image.new("RGB", (w, h), (0, 0, 0))
 for i, arg in enumerate(sys.argv[1:5]):
-	masked = gen_masked(arg, masks[i])
-	final.paste(masked, (0, 0), masked)
+    masked = gen_masked(arg, masks[i])
+    final.paste(masked, (0, 0), masked)
 
 
 if backg:
-    bg.paste(round_corners(final), (int(m/2), int(m/2)), round_corners(final))
+    bg.paste(round_corners(final), (int(m / 2), int(m / 2)), round_corners(final))
     final = bg
 if show:
-	final.show()
+    final.show()
 else:
-	if not os.path.exists(OUT_DIR):
-		os.makedirs(OUT_DIR)
-	final.save(OUT_DIR + 'res.png')
+    if not os.path.exists(OUT_DIR):
+        os.makedirs(OUT_DIR)
+    final.save(OUT_DIR + "res.png")
