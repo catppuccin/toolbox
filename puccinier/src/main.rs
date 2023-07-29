7387701f61759fe8f23405398f1158d0f66e8f50
Sam Nystrom <sam@samnystrom.dev>
2023-07-28 22:02:38 -0400

    fix(puccinier)!: minimize dependencies
    Replace serde with miniserde since performance and flexibility are both
    very non-critical for this program (all of the JSON parsing is done
    once, on startup).
    
    Replace clap with xflags since the program's arg parsing requirements
    are very simple. This removes a bunch of dependencies, greatly improves
    compile times, and shaves ~300KB off of the release binary.
    
    BREAKING CHANGE: The --source CLI option is now a positional arg.
    
    BREAKING CHANGE: The --output CLI option is now repeatable, rather than
    accepting space-delimited values.

--- puccinier/src/main.rs
+++ puccinier/src/main.rs
@@ -1,119 +1,91 @@
-mod palette;
-
-use clap::{arg, builder::PossibleValuesParser, value_parser, ErrorKind};
-use regex::{Match, Regex};
 use std::{
     fs::File,
-    io::{BufRead, BufReader, BufWriter, Write},
+    io::{self, BufRead, BufReader, BufWriter, Write},
     path::PathBuf,
+    collections::HashMap,
 };
+use miniserde::json;
 
-use palette::{Color, Palette, COLOR_FROM_VARIANT, VARIANT_FROM_COLOR};
+pub type Color = HashMap<String, String>;
+pub type Palette = HashMap<String, Color>;
 
-fn main() {
-    let mut cmd = clap::command!()
-        .arg( arg!(-s --source <FILE> "Set the source file to convert")
-			.value_parser(value_parser!(PathBuf))
-			.required(true))
-        .arg( arg!(-o --output <TYPES> "Set the themes (space-separated) to generate from the source file")
-			.value_parser(PossibleValuesParser::new(["latte", "frappe", "macchiato", "mocha"]))
-			.takes_value(true)
-			.multiple_values(true)
-			.required(true));
 
-    let matches = cmd.clone().get_matches();
+fn main() -> io::Result<()> {
+    let mut color_from_variant: HashMap<&'static str, Palette> = HashMap::new();
+    color_from_variant.insert("v1", json::from_str(include_str!("../palettes/v1/converted.json")).unwrap());
+    color_from_variant.insert("latte", json::from_str(include_str!("../palettes/v2/latte.json")).unwrap());
+    color_from_variant.insert("frappe", json::from_str(include_str!("../palettes/v2/frappe.json")).unwrap());
+    color_from_variant.insert("macchiato", json::from_str(include_str!("../palettes/v2/macchiato.json")).unwrap());
+    color_from_variant.insert("mocha", json::from_str(include_str!("../palettes/v2/mocha.json")).unwrap());
 
-    let source_path: &PathBuf = match matches.get_one("source") {
-        Some(path) => path,
-        None => cmd
-            .error(ErrorKind::ValueValidation, "Failed to read source argument")
-            .exit(),
-    };
-
-    let source_file = match File::options().read(true).write(true).open(source_path) {
-        Ok(file) => file,
-        Err(e) => cmd
-            .error(ErrorKind::Io, format!("Failed to open source file: {e}"))
-            .exit(),
-    };
-
-    let output_themes: Vec<&String> = match matches.get_many::<String>("output") {
-        Some(output_themes) => output_themes.collect(),
-        None => cmd
-            .error(ErrorKind::ValueValidation, "Failed to read output argument")
-            .exit(),
-    };
-
-    let mut writers: Vec<BufWriter<File>> = output_themes
-        .iter()
-        .map(|theme| {
-            let mut path = source_path.with_file_name(theme);
-            if let Some(extension) = source_path.extension() {
-                path = path.with_extension(extension);
+    let mut variant_from_color: HashMap<String, [String; 3]> = HashMap::new();
+    for (variant, labels) in &color_from_variant {
+        for (label, colors) in labels.iter() {
+            for (format, value) in colors.iter() {
+                variant_from_color.insert(value.to_owned(), [variant.to_string(), label.to_string(), format.to_string()]);
             }
-            BufWriter::new(
-                match File::create(path) {
-                    Ok(file) => file,
-                    Err(e) => cmd
-                        .error(ErrorKind::Io, format!("Failed to create output file: {e}"))
-                        .exit(),
-                },
-            )
-        })
-        .collect();
-
-    lazy_static::lazy_static! {
-        static ref HEX: Regex = Regex::new(r"(?i)#([A-F0-9]{6}|[A-F0-9]{3})").unwrap();
-        static ref RGB: Regex = Regex::new(r"(?i)rgba?\(.+\)").unwrap();
-        static ref HSL: Regex = Regex::new(r"(?i)hsla?\(.+\)").unwrap();
+        }
     }
 
-    for line in BufReader::new(&source_file).lines() {
-        let line = match line {
-            Ok(line) => line,
-            Err(e) => {
-                cmd.error(ErrorKind::Io, format!("Failed to read line: {e}"))
-                    .exit();
+    xflags::xflags! {
+        /// Generate the other Catppuccin flavours off a template file written in one of them
+        cmd puccinier {
+            /// Set the source file to convert
+            required source: PathBuf
+            /// Set the themes to generate from the source file
+            repeated -o, --output type: String
+        }
+    };
+
+    let flags = Puccinier::from_env_or_exit();
+
+
+    let source_file = File::open(&flags.source)?;
+
+    let output_themes: Vec<String> = flags.output;
+
+    let writers: io::Result<Vec<BufWriter<File>>> = output_themes
+        .iter()
+        .map(|theme| -> io::Result<BufWriter<File>> {
+            let mut path = flags.source.with_file_name(theme);
+            if let Some(extension) = flags.source.extension() {
+                path = path.with_extension(extension);
             }
-        };
+            Ok(BufWriter::new(File::create(path)?))
+        })
+        .collect();
+    let mut writers = writers?;
+
+
+    let regex = regex::Regex::new(r"(?i)#([A-F0-9]{6}|[A-F0-9]{3})|rgba?\(.+\)|hsla?\(.+\)").unwrap();
+    for line in BufReader::new(&source_file).lines() {
+        let line = line?;
 
         for (theme, writer) in output_themes.iter().zip(&mut writers) {
             let mut copy = line.clone();
 
-            HEX.find_iter(&line)
-                .chain(RGB.find_iter(&line))
-                .chain(HSL.find_iter(&line))
-                .filter_map(|item: Match| -> Option<([&String; 3], &str)> {
+            regex.find_iter(&line)
+                .filter_map(|item| {
                     let replacement: &str = item.as_str();
-
-                    VARIANT_FROM_COLOR
+                    variant_from_color
                         .get(&replacement.to_lowercase())
                         .cloned()
                         .zip(Some(replacement))
                 })
                 .for_each(|(lookup, replacement)| {
-                    let label: &Palette = COLOR_FROM_VARIANT.get(theme.as_str()).unwrap();
-                    let color_format: &Color = label.get(lookup[1]).unwrap();
-                    let color_value: &String = color_format.get(lookup[2]).unwrap();
+                    let label: &Palette = color_from_variant.get(theme.as_str()).unwrap();
+                    let color_format: &Color = label.get(&lookup[1]).unwrap();
+                    let color_value: &String = color_format.get(&lookup[2]).unwrap();
 
                     copy = copy.replace(replacement, color_value);
                 });
 
-            if let Err(e) = writeln!(writer, "{}", copy) {
-                cmd.error(ErrorKind::Io, format!("Failed to write line: {e}"))
-                    .print()
-                    .unwrap();
-            }
+            writeln!(writer, "{}", copy)?;
         }
     }
 
     for mut writer in writers {
-        if let Err(e) = writer.flush() {
-            cmd.error(
-                ErrorKind::Io,
-                format!("Failed to flush writer: {e}. Changes will be dropped."),
-            )
-            .exit();
-        }
+        writer.flush()?;
     }
+    Ok(())
 }
