d66ec8d984887c4d417166d2180bcf249e11a318
winston <hey@winston.sh>
2023-10-28 01:19:43 +0200

    chore(catwalk): release as 1.0.0
    Release-As: 1.0.0

--- catwalk/cli.ts
+++ catwalk/cli.ts
@@ -14,7 +14,7 @@ import {
 
 const { args, options } = await new Command()
   .name("catwalk")
-  .version("0.1.0")
+  .version("1.0.0")
   .description(
     "A sweet program that takes in four showcase images and displays them all at once. (JS port)",
   )
