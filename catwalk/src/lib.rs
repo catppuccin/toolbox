9d95725b07d8a9f9165a58782091d5d3f6d4e4c9
winston <hey@winston.sh>
2023-11-03 19:59:00 +0100

    feat: add 4x1 layout
    Closes #20

--- catwalk/src/lib.rs
+++ catwalk/src/lib.rs
@@ -22,6 +22,12 @@ pub enum Layout {
     Composite,
     Stacked,
     Grid,
+    FourByOne,
+}
+
+enum GridLayouts {
+    TwoByTwo,
+    FourByOne,
 }
 
 #[derive(thiserror::Error, Debug)]
@@ -220,7 +226,7 @@ impl Magic {
     }
 
     /// Creates a grid image.
-    fn gen_grid(&self) -> Image<Rgba> {
+    fn gen_grid(&self, layout: &GridLayouts) -> Image<Rgba> {
         // Round images
         let gap = self.gap;
         let rounded: Vec<Image<Rgba>> = self
@@ -228,17 +234,31 @@ impl Magic {
             .iter()
             .map(|x| self.rounding_mask.mask(x))
             .collect();
+
         // Create final
-        let mut result = Image::new(
-            (self.width * 2) + (gap * 3),
-            (self.height * 2) + (gap * 3),
-            Rgba::transparent(),
-        )
+        let mut result = match layout {
+            GridLayouts::TwoByTwo => Image::new(
+                (self.width * 2) + (gap * 3),
+                (self.height * 2) + (gap * 3),
+                Rgba::transparent(),
+            ),
+            GridLayouts::FourByOne => Image::new(
+                (self.width * 4) + (gap * 5),
+                self.height + (gap * 2),
+                Rgba::transparent(),
+            ),
+        }
         .with_overlay_mode(OverlayMode::Merge);
         // calculate the top left coordinates for each image, and paste
         rounded.iter().enumerate().for_each(|(i, img)| {
-            let x = i % 2;
-            let y = i / 2;
+            let x = match layout {
+                GridLayouts::FourByOne => i % 4,
+                GridLayouts::TwoByTwo => i % 2,
+            };
+            let y = match layout {
+                GridLayouts::FourByOne => 0,
+                GridLayouts::TwoByTwo => i / 2,
+            };
             result.paste(
                 gap + (self.width + gap) * x as u32,
                 gap + (self.height + gap) * y as u32,
@@ -266,7 +286,8 @@ impl Magic {
         match self.layout {
             Layout::Composite => self.gen_composite(),
             Layout::Stacked => self.gen_stacked(),
-            Layout::Grid => self.gen_grid(),
+            Layout::Grid => self.gen_grid(&GridLayouts::TwoByTwo),
+            Layout::FourByOne => self.gen_grid(&GridLayouts::FourByOne),
         }
     }
 
