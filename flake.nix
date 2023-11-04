ede1be289ff7ab623cb8343502d0395a880d8f5d
winston <hey@winston.sh>
2023-11-04 20:41:26 +0100

    ci(nix): add cachix

--- flake.nix
+++ flake.nix
@@ -49,4 +49,9 @@
 
     formatter = forEachSystem (pkgs: pkgs.alejandra);
   };
+
+  nixConfig = {
+    extra-substituters = ["https://catppuccin.cachix.org"];
+    extra-trusted-public-keys = ["catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="];
+  };
 }
