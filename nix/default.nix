c4334158cda27c8c0446d3207b527acfb6879d8c
winston <hey@winston.sh>
2023-12-23 15:00:07 +0100

    refactor: flake

--- nix/default.nix
+++ nix/default.nix
@@ -1,70 +1,40 @@
-{
-  pkgs,
-  version ? "dirty",
-}: let
-  mkRustPkg = {
-    pname,
-    description,
-    ...
-  } @ args:
-    pkgs.rustPlatform.buildRustPackage ({
-        inherit pname version;
-        src = pkgs.nix-gitignore.gitignoreSourcePure [../.gitignore] ../.;
+{pkgs}: let
+  inherit (pkgs) lib;
+  inherit (builtins) readFile fromTOML;
+  workspaceCargoToml = fromTOML (readFile ../Cargo.toml);
+
+  mkRustPkg = {pname, ...} @ args: let
+    memberCargoToml = fromTOML (readFile ../${pname}/Cargo.toml);
+  in
+    pkgs.rustPlatform.buildRustPackage (lib.recursiveUpdate {
+        inherit pname;
+        inherit (memberCargoToml.package) version;
+        src = pkgs.nix-gitignore.gitignoreSource [] ../.;
 
         cargoLock.lockFile = ../Cargo.lock;
         buildAndTestSubdir = pname;
 
-        meta = with pkgs.lib; {
-          inherit description;
+        meta = {
+          description = memberCargoToml.package.description or null;
           homepage = "https://github.com/catppuccin/toolbox/tree/main/${pname}";
-          license = licenses.mit;
-          maintainers = with maintainers; [rubyowo];
+          license = lib.licenses.mit;
+          mainProgram = pname;
         };
       }
-      // args);
-
-  nodePkgs = [
-    {
-      pname = "docpuccin";
-      description = "Fetch health files needed per project type";
-    }
-    {
-      pname = "inkcat";
-      description = "Display Catppuccin flavors in your terminal";
-    }
-    {
-      pname = "contrast_test";
-      description = "Test Catppuccin's flavors compliance with modern web contrast standards";
-    }
-  ];
-
-  rustPkgs = [
-    {
-      pname = "puccinier";
-      description = "Generate the other Catppuccin flavors off a template file written in one of them";
-    }
-    rec {
-      pname = "catwalk";
-      description = "Generate a preview as a single composite screenshot for the four flavors";
-
+      args);
+  rustPkgOverrides = {
+    catwalk = {
       nativeBuildInputs = with pkgs; [installShellFiles pkg-config];
       buildInputs = with pkgs; [libwebp];
 
       postInstall = ''
-        installShellCompletion --cmd ${pname} \
-          --bash <($out/bin/${pname} completion bash) \
-          --fish <($out/bin/${pname} completion fish) \
-          --zsh <($out/bin/${pname} completion zsh)
+        installShellCompletion --cmd catwalk \
+          --bash <($out/bin/catwalk completion bash) \
+          --fish <($out/bin/catwalk completion fish) \
+          --zsh <($out/bin/catwalk completion zsh)
       '';
-    }
-    {
-      pname = "whiskers";
-      description = "Soothing port creation tool for the high-spirited!";
-    }
-  ];
+    };
+  };
+  rustPkgs = lib.genAttrs workspaceCargoToml.workspace.members (pname: mkRustPkg ({inherit pname;} // rustPkgOverrides.${pname} or {}));
 in
-  builtins.listToAttrs (builtins.map ({...} @ args: {
-      name = args.pname;
-      value = mkRustPkg args;
-    })
-    rustPkgs)
+  rustPkgs
