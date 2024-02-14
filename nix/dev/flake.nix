c390e760dec18bbd1abc136ef0ad878b1bb77afc
winston <hey@winston.sh>
2024-02-14 02:16:53 +0100

    build(nix): rust-overlay -> fenix

--- nix/dev/flake.nix
+++ nix/dev/flake.nix
@@ -3,10 +3,8 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-    rust-overlay.url = "github:oxalica/rust-overlay";
-    rust-overlay.inputs.nixpkgs.follows = "nixpkgs";
-    rust-overlay.inputs.flake-utils.follows = "flake-utils";
+    fenix.url = "https://flakehub.com/f/nix-community/fenix/0.1.*.tar.gz";
+    fenix.inputs.nixpkgs.follows = "nixpkgs";
     call-flake.url = "github:divnix/call-flake";
   };
 
@@ -17,33 +15,43 @@
     ...
   } @ inputs: let
     systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
-    overlays = [(import inputs.rust-overlay)];
-    forEachSystem = fn: nixpkgs.lib.genAttrs systems (system: fn (import nixpkgs {inherit overlays system;}));
+    overlays = [inputs.fenix.overlays.default];
+    inherit (nixpkgs) lib;
+    forEachSystem = fn: lib.genAttrs systems (system: fn (import nixpkgs {inherit overlays system;}));
     mainFlake = call-flake ../..;
   in {
     inherit (mainFlake) checks formatter packages;
 
-    devShells = forEachSystem (pkgs: {
+    devShells = forEachSystem (pkgs: let
+      inherit (pkgs.stdenv) isDarwin;
+      rust-toolchain = pkgs.fenix.combine [
+        pkgs.fenix.complete.toolchain
+        pkgs.rust-analyzer
+        pkgs.fenix.targets.wasm32-unknown-unknown.latest.rust-std
+      ];
+    in {
       default = pkgs.mkShell {
-        buildInputs = with pkgs; [
-          self.formatter.${pkgs.system}
-          deno
-          (rust-bin.stable.latest.default.override {
-            extensions = ["rust-analyzer" "rust-src"];
-            targets = ["wasm32-unknown-unknown"];
-          })
-          # wasm + publishing to npm
-          binaryen
-          nodejs
-          wasm-bindgen-cli
-          wasm-pack
-        ];
+        buildInputs = with pkgs;
+          [
+            self.formatter.${pkgs.system}
+            rust-toolchain
+            deno
+            # wasm + publishing to npm
+            binaryen
+            nodejs
+            wasm-bindgen-cli
+            wasm-pack
+            # wasm-bindgen can require lcurl to build
+            curl
+          ]
+          ++ lib.optionals isDarwin [libiconv];
+        env.RUST_SRC_PATH = "${rust-toolchain}/lib/rustlib/src/rust/library";
       };
     });
   };
 
   nixConfig = {
-    extra-substituters = ["https://catppuccin.cachix.org"];
-    extra-trusted-public-keys = ["catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="];
+    extra-substituters = ["https://catppuccin.cachix.org" "https://nix-community.cachix.org"];
+    extra-trusted-public-keys = ["catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU=" "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
   };
 }
