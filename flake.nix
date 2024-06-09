21381966172cfaafa8063c2c973e2126535ac730
Hammy <58985301+sgoudham@users.noreply.github.com>
2024-06-10 00:31:45 +0100

    refactor!: move `catwalk` to `catppuccin/catwalk` (#245)
    * refactor!: move `catwalk` to `catppuccin/catwalk`
    
    * build(nix): refactor flake
    
    ---------
    
    Co-authored-by: isabelroses <isabel@isabelroses.com>

--- flake.nix
+++ flake.nix
@@ -3,26 +3,24 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  }: let
-    systems = ["aarch64-darwin" "aarch64-linux" "x86_64-darwin" "x86_64-linux"];
-    forEachSystem = fn: lib.genAttrs systems (system: fn (import nixpkgs {inherit system;}));
-    inherit (nixpkgs) lib;
-  in {
-    checks = forEachSystem (pkgs: self.packages.${pkgs.system});
-    packages = forEachSystem (pkgs: lib.filterAttrs (_: v: lib.isDerivation v) (pkgs.callPackage ./nix { }) // { whiskers = throw "This package has been moved to x `github:catppuccin/whiskers`"; } );
-    overlays.default = final: prev: {
-      catppuccin-catwalk = (prev.callPackage ./nix {}).catwalk;
-      catppuccin-whiskers = throw "This package has been moved to x `github:catppuccin/whiskers`";
+  outputs =
+    { nixpkgs, ... }:
+    let
+      forAllSystems =
+        function:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (
+          system: function nixpkgs.legacyPackages.${system}
+        );
+    in
+    {
+      packages = forAllSystems (_: {
+        whiskers = throw "This package has been moved to `github:catppuccin/whiskers`";
+        catwalk = throw "This package has been moved to `github:catppuccin/catwalk`";
+      });
+      
+      overlays.default = _: _: {
+        catppuccin-catwalk = throw "This package has been moved to `github:catppuccin/catwalk`";
+         catppuccin-whiskers = throw "This package has been moved to `github:catppuccin/whiskers`";
+};
     };
-    formatter = forEachSystem (pkgs: pkgs.alejandra);
-  };
-
-  nixConfig = {
-    extra-substituters = ["https://catppuccin.cachix.org"];
-    extra-trusted-public-keys = ["catppuccin.cachix.org-1:noG/4HkbhJb+lUAdKrph6LaozJvAeEEZj4N732IysmU="];
-  };
 }
